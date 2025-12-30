da0f641894f8a5c338448186144c12b210eebc5a
wellingtonsilva-otimize <wellington.silva.otimize@fogas.com.br>
2025-12-30 17:21:43 +0000

    Adicionado configuracao do Swagger

--- tasko-vendedor/src/main/java/br/com/wasistemas/tasko/vendedor/adapter/in/web/request/AdicionarVendedorRequest.java
+++ tasko-vendedor/src/main/java/br/com/wasistemas/tasko/vendedor/adapter/in/web/request/AdicionarVendedorRequest.java
@@ -1,17 +1,35 @@
 package br.com.wasistemas.tasko.vendedor.adapter.in.web.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 @Value
+@Schema(description = "Dados para Adicioanr um Vendedor")
 public class AdicionarVendedorRequest {
-    Long id;
+    @Schema(description = "Código do Vendedor", example = "0009-1")
     String codigoVendedor;
+
+    @Schema(description = "Nome completo", example = "João Silva")
     String nomeVendedor;
+
+    @Schema(description = "Número do CPF", example = "999.999.999-99")
     String numeroCPF;
+
+    @Schema(description = "E-mail principal do Vendedor", example = "joao.silva@email.com")
     String email;
+
+    @Schema(description = "Número de Telefone", example = "(99) 99999-9999")
     String numeroTelefone;
+
+    @Schema(description = "Valor da Meta Mensal", example = "99.99")
     Float valorMetaMensal;
+
+    @Schema(description = "Percentual de Comissão", example = "9.9")
     Float percentualComissao;
+
+    @Schema(description = "Id. do Supervisor Responsável", example = "1")
     Long supervisorId;
+
+    @Schema(description = "Id. do Território", example = "2")
     Long territorioId;
 }
